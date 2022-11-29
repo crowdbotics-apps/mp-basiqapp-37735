1202fe31bc8306341cc86c55cad2505aeee3cc35
Matt Pavelko <matt@crowdbotics.com>
2022-11-29 09:54:52 -0500

    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,13 @@
+const styles = StyleSheet.create({});
 import { StyleSheet } from "react-native";
-
 export const globalOptions = {
-  name: "mp_basiqapp_37735",
-  url: "https://mp_basiqapp_37735.botics.co",
-  api: "https://mp_basiqapp_37735.botics.co/api/v1"
-}
+  "name": "mp_basiqapp_37735",
+  "url": "https://mp_basiqapp_37735.botics.co",
+  "api": "https://mp_basiqapp_37735.botics.co/api/v1"
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     "copy": "Routes available!"
-  }
-}
+  },
+  "@modules/basiqmodule": {}
+};
